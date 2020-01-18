6ccfcd2f083de6132bc72ccbd8cddeeebe77985f
Anushka Singh <anushka318singh@gmail.com>
2020-01-18 14:15:13 +0530

    new activity for shake detection

---
+++ app/src/main/java/com/example/iris/ActivateService.java
@@ -0,0 +1,14 @@
+package com.example.iris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivateService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activate_service);
+    }
+}
