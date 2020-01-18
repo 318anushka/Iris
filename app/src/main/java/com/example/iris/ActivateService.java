90f486c27a001a70c2ab8aeeaa9c0ce12c0fe929
Anushka Singh <anushka318singh@gmail.com>
2020-01-18 14:45:26 +0530

    shake detection

--- app/src/main/java/com/example/iris/ActivateService.java
+++ app/src/main/java/com/example/iris/ActivateService.java
@@ -2,13 +2,57 @@ package com.example.iris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ActivateService extends AppCompatActivity {
+
+    private SensorManager sm;
+    private float accelerationValue;
+    private float accelerationLast;
+    private float shake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activate_service);
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) , SensorManager.SENSOR_DELAY_NORMAL);
+
+        accelerationLast = SensorManager.GRAVITY_EARTH;
+        accelerationValue = SensorManager.GRAVITY_EARTH;
+        shake = 0.00f;
+
     }
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            accelerationLast = accelerationValue;
+            accelerationValue = (float)Math.sqrt((double) x*x + y*y + z*z);
+            float diff = accelerationValue - accelerationLast;
+             shake = shake*0.9f + diff;
+
+             if(shake>12){
+                 Toast.makeText(ActivateService.this , "emergency it is" , Toast.LENGTH_LONG).show();
+             }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/iris/HomeActivity.java
+++ app/src/main/java/com/example/iris/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.iris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +13,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button1 = findViewById(R.id.activateservice);
+        Button button2 = findViewById(R.id.stayconnected);
+        Button button3 = findViewById(R.id.updatecontacts);
+        Button button4 = findViewById(R.id.tips);
+        Button button5 = findViewById(R.id.help);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent1 = new Intent(HomeActivity.this , ActivateService.class);
+                startActivity(intent1);
+            }
+        });
+
+
     }
 
 }
