1397f2538572bb91f83579238dea7733b5d6c1b7
Sweety <16jainsweety@gmail.com>
2020-01-19 00:12:04 +0530

    emergency contacts added

--- app/src/main/java/com/example/iris/HomeActivity.java
+++ app/src/main/java/com/example/iris/HomeActivity.java
@@ -30,6 +30,14 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent new_intent=new Intent(HomeActivity.this,UpdateActivity.class);
+                startActivity(new_intent);
+            }
+        });
+
 
     }
 
