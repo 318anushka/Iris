071e9aae4a51327c90063d44d3b1c3413e5a3a95
Sweety <16jainsweety@gmail.com>
2020-01-19 00:41:06 +0530

    getting reference of database node for data retrieval

--- app/src/main/java/com/example/iris/ShowNumberActivity.java
+++ app/src/main/java/com/example/iris/ShowNumberActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,5 +26,20 @@ public class ShowNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_number);
+
+
+        mListView=findViewById(R.id.myList);
+        database=FirebaseDatabase.getInstance();
+        // Firebase.setAndroidContext(this);
+
+        //  mRef=new Firebase ("https://make-her-safe.firebaseio.com/user/uID");
+
+
+        uID= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mRef=database.getReference().child(uID);
+
+
+
+        arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mArrayList);
     }
 }
