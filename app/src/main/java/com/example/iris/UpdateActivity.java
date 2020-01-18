18a0844e1643c02538e9fbe13a69d318c8e7e889
Sweety <16jainsweety@gmail.com>
2020-01-19 00:36:05 +0530

    initialization of variables used

--- app/src/main/java/com/example/iris/UpdateActivity.java
+++ app/src/main/java/com/example/iris/UpdateActivity.java
@@ -3,8 +3,21 @@ package com.example.iris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    private EditText mTextView;
+    private Button add_btn;
+    private Button show_btn;
+
+    //  private String mNum=null;
+    private String uID;
+
+    private Firebase mref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
