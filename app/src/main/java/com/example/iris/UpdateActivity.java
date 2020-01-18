d2bdddccc5ad4542d602736188228b40854f1e2d
Sweety <16jainsweety@gmail.com>
2020-01-19 00:37:13 +0530

    setting up reference to firebase database node

--- app/src/main/java/com/example/iris/UpdateActivity.java
+++ app/src/main/java/com/example/iris/UpdateActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -23,5 +24,15 @@ public class UpdateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        mTextView=findViewById(R.id.num_text);
+        add_btn=findViewById(R.id.add_btn);
+        show_btn=findViewById(R.id.show_btn);
+
+
+        Firebase.setAndroidContext(this);
+
+        mref=new Firebase("https://iris-8e542.firebaseio.com/");
+        uID= FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 }
